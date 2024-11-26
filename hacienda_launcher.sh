69ae9c9ff901611195a75a3c873a0337b97e5d27
streethagore <srivaud972@gmail.com>
2024-11-26 01:03:22 +0100

    Tracking gradient metrics

--- hacienda_launcher.sh
+++ hacienda_launcher.sh
@@ -9,7 +9,7 @@ output_dir='logs/iclr2025-async-rebuttal'  # output directory for logs and check
 
 # command parameters
 dataset='cifar10'
-model='revnet18'
+model='revnet34'
 synchronous='false'
 store_vjp='false'
 store_input='false'
@@ -21,7 +21,7 @@ batch_size=64
 wandb_project='iclr2025-async-rebuttal-grad-approx'
 
 # testing a single job
-for accumulation_steps in 1 4 16; do
+for accumulation_steps in 1; do
   for dataset in 'cifar10' 'cifar100'; do
     sbatch hacienda_script.sh $gpu_type $output_dir $dataset $model $synchronous $store_vjp $store_input $store_param $approximate_input $accumulation_steps $lr $batch_size $wandb_project
   done
