8ea42d44b149081ce9e9ba2f3f588331175f1807
streethagore <srivaud972@gmail.com>
2024-11-26 21:08:17 +0100

    Experiments with RevNet101

--- revnet101_launcher.sh
+++ revnet101_launcher.sh
@@ -8,7 +8,7 @@ gpu_type='none'  # 'a100', 'v100', 'v100-16g', 'v100-32g'
 output_dir='logs/iclr2025-async-rebuttal'  # output directory for logs and checkpoints
 
 # command parameters
-dataset='cifar10'
+dataset='cifar100'
 model='revnet101'
 synchronous='true'
 store_vjp='false'
@@ -21,6 +21,12 @@ batch_size=64
 wandb_project='iclr2025-async-revnet101'
 
 # testing a single job
+accumulation_steps=1
+synchronous='true'
+sbatch revnet101_script.sh $gpu_type $output_dir $dataset $model $synchronous $store_vjp $store_input $store_param $approximate_input $accumulation_steps $lr $batch_size $wandb_project
+
+accumulation_steps=1
+synchronous='false'
 sbatch revnet101_script.sh $gpu_type $output_dir $dataset $model $synchronous $store_vjp $store_input $store_param $approximate_input $accumulation_steps $lr $batch_size $wandb_project
 
 accumulation_steps=16
