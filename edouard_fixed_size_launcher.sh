0f4f74ed38e86c67afc6cd81de9004cb20d7efe9
streethagore <srivaud972@gmail.com>
2024-12-12 19:05:36 +0100

    Experiments with buffer quantization

--- edouard_fixed_size_launcher.sh
+++ edouard_fixed_size_launcher.sh
@@ -12,7 +12,7 @@ lr=0.1
 # testing a single job
 for dataset in 'cifar10' 'cifar100'; do
   for accumulation_steps in 1 2 4; do
-    for lr in 0.05 0.025; do
+    for lr in 0.0125 0.00625; do
       for hidden_size in 64 128 256; do
         for synchronous in 'true' 'false'; do
           command="sbatch edouard_fixed_size_script.sh $dataset $n_layers $hidden_size $synchronous $accumulation_steps $lr"
