d6051a7a78588a8a6e475308b3da39072314cfcd
streethagore <srivaud972@gmail.com>
2024-12-13 22:43:49 +0100

    Experiments with buffer quantization

--- fixed_size_launcher.sh
+++ fixed_size_launcher.sh
@@ -12,7 +12,7 @@ lr=0.1
 # testing a single job
 for dataset in 'cifar10' 'cifar100'; do
   for accumulation_steps in 8 16; do
-    for lr in 0.025 0.0125 0.00625; do
+    for lr in 0.1 0.05; do
       for hidden_size in 64 128 256; do
         for synchronous in 'true' 'false'; do
           command="sbatch fixed_size_script.sh $dataset $n_layers $hidden_size $synchronous $accumulation_steps $lr"
