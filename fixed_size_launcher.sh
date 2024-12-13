5a9e39332e36ec402f547af8b7025cee6c31da50
streethagore <srivaud972@gmail.com>
2024-12-13 22:56:23 +0100

    Experiments with fully-reversible architecture

--- fixed_size_launcher.sh
+++ fixed_size_launcher.sh
@@ -3,7 +3,7 @@
 # ----- Parameters -----
 # command parameters
 dataset='cifar10'
-n_layers=5
+n_layers=10
 hidden_size=256
 synchronous='true'
 accumulation_steps=1
@@ -11,7 +11,7 @@ lr=0.1
 
 # testing a single job
 for dataset in 'cifar10' 'cifar100'; do
-  for accumulation_steps in 8 16; do
+  for accumulation_steps in 1 2 4 8 16; do
     for lr in 0.1 0.05; do
       for hidden_size in 64 128 256; do
         for synchronous in 'true' 'false'; do
