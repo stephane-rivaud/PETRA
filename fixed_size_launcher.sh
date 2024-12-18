4c7d30ed4133cd3244fe8910c43d2de6031492fb
streethagore <srivaud972@gmail.com>
2024-12-18 17:33:41 +0100

    Experiments with fully-reversible architecture

--- fixed_size_launcher.sh
+++ fixed_size_launcher.sh
@@ -7,18 +7,16 @@ n_layers=10
 hidden_size=256
 synchronous='true'
 accumulation_steps=1
-lr=0.1
+lr=0.2
 
-# testing a single job
+# launching experiments
 for dataset in 'cifar10' 'cifar100'; do
   for accumulation_steps in 1 2 4 8 16; do
-    for lr in 0.0125; do
+    for n_layers in 5 10; do
       for hidden_size in 64 128 256; do
-        for synchronous in 'true' 'false'; do
-          command="sbatch fixed_size_script.sh $dataset $n_layers $hidden_size $synchronous $accumulation_steps $lr"
-          echo $command
-          eval $command
-        done
+        command="sbatch fixed_size_script.sh $dataset $n_layers $hidden_size $synchronous $accumulation_steps $lr"
+        echo $command
+        eval $command
       done
     done
   done
